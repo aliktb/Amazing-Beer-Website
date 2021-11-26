89890e9883c60943c45762332b9d6d9f1ae0f798
samkazshel <samsheldon779@gmail.com>
2021-11-26 12:13:42 +0000

    created beer persistence file

---
+++ Backend/persistence/beer.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const { Schema, model } = require("mongoose");
+
+const beerSchema = new Schema({
+  id: {
+    type: Number,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    maxlength: 150,
+  },
+  image_url: {
+    type: String,
+  },
+  abv: {
+    type: Number,
+  },
+  alcochol_free: Boolean,
+  type: {
+    type: String,
+  },
+});
+
+const Beer = model("Beer", beerSchema);
+
+module.exports = { Beer: Beer };
